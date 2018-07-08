28a1e75c3f34398aae18e556aae0ac5574d1c8a1
Edward Fox <edwardkenfox@gmail.com>
2018-07-08 22:04:19 +0900

    Use microphone and allow sending raw audio

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,15 +2,76 @@
   <div id="app">
     <form action="http://localhost:8081/" method="post" enctype="multipart/form-data">
       <img src="./assets/logo.png">
-      <input id="file" type="file" name="file" value="">
-      <button id="submit" type="submit">送信</button>
+      <audio id="player" controls></audio>
+      <button v-if="!isRecording" @click.prevent="startRecording">Start</button>
+      <button v-else @click.prevent="stopRecording">Stop</button>
+      <input id="file" type="file" name="file" accept="audio/*" capture>
+      <button id="submit" type="submit" @click.prevent="submit">送信</button>
     </form>
   </div>
 </template>
 <script>
-// Vueインスタンスの存在意義...
 export default {
   name: 'app',
+
+  data () {
+    return {
+      isRecording: false,
+      audioChunks: [],
+      mediaRecorder: null,
+      audioBlob: null
+    }
+  },
+
+  methods: {
+    validateFilePresence (ev) {
+      if (!this.$el.querySelector('#file').value) {
+        ev.preventDefault()
+        return false
+      }
+      return true
+    },
+    startRecording () {
+      const player = document.getElementById('player')
+      player.src = null
+
+      this.isRecording = true
+
+      navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+        .then(stream => {
+          console.log('start recording')
+
+          this.mediaRecorder = new MediaRecorder(stream)
+          this.mediaRecorder.start()
+
+          this.mediaRecorder.addEventListener('dataavailable', event => {
+            this.audioChunks.push(event.data)
+          })
+
+          this.mediaRecorder.addEventListener('stop', () => {
+            this.audioBlob = new Blob(this.audioChunks)
+            const audioUrl = URL.createObjectURL(this.audioBlob)
+            player.src = audioUrl
+          })
+        })
+    },
+    stopRecording () {
+      if (!this.mediaRecorder) return false
+      console.log('stop recording')
+      this.isRecording = false
+      this.mediaRecorder.stop()
+    },
+    submit () {
+      var fd = new FormData()
+      fd.append('file', this.audioBlob, 'myaudio.txt')
+
+      fetch('http://localhost:8081/', {
+        method: 'POST',
+        body: fd
+      })
+    }
+  },
+
   mounted () {
     window.vm = this
   }
